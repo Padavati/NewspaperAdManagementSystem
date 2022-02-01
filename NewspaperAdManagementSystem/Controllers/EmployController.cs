91c43ecefccc439df3a594d99ce2be58819feeca
Padavati <lavanyavaralu07399@gmail.com>
2022-02-01 21:25:40 +0530

    Add project files.

---
+++ NewspaperAdManagementSystem/Controllers/EmployController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewspaperAdManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewspaperAdManagementSystem.Controllers
+{
+    public class EmployController : Controller
+    {
+        private readonly ConnectionStringClass _connection;
+        public EmployController(ConnectionStringClass connection)
+        {
+            _connection = connection;
+        }
+        public IActionResult Index()//Select*from EmployDetails
+        {
+            var employdetails = _connection.EmployDetails.ToList();
+            return View(employdetails);
+        }
+        public IActionResult EmployDetails()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EmployDetails(EmployDetailsClass employDetailsClass)//Insertion into EmployDetails values
+        {
+            _connection.Add(employDetailsClass);
+            _connection.SaveChanges();
+            ViewBag.message = "Saved Successfully";
+            return View(employDetailsClass);
+        }
+       
+        public IActionResult EditEmployDetails(int EmpID)//select*from EmployDetails where EmpID
+        {
+            var employdetails = _connection.EmployDetails.Find(EmpID);                  
+            ViewBag.message = "Edit operation done successfully";
+            return View(employdetails);
+        }
+    }
+}
---
+++ NewspaperAdManagementSystem/Models/EmployDetailsClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewspaperAdManagementSystem.Models
+{
+    public class EmployDetailsClass
+    {
+        [Key]
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string EmpContactNo { get; set; }
+        public string EmailID { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Pwd { get; set; }
+        public string Role { get; set; }
+    }
+}
